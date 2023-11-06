27d297b554741aba0360c89adefa33537f19ebc8
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-11-06 23:41:11 +0200

    Make required changes

--- class/person.rb
+++ class/person.rb
@@ -1,20 +1,20 @@
 class Person
   attr_accessor :id, :name, :age
 
-  def initialize(id, age, name: 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    @parent_permission || of_age?
   end
 
   private
 
   def of_age?
     @age >= 18
-  end
-
-  def can_use_services?
-    @parent_permission || of_age?
   end
 end
