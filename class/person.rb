3b2830933ea9018e5f99156eb7933b0bd7fa27af
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-11-08 19:11:15 +0200

    Fix linter errors

--- class/person.rb
+++ class/person.rb
@@ -1,7 +1,12 @@
-class Person
+require_relative 'nameable'
+require_relative 'base_decorator'
+require_relative 'trim_decorator'
+require_relative 'capitalize_decorator'
+class Person < Nameable
   attr_accessor :id, :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = rand(1..1000)
     @name = name
     @age = age
@@ -12,9 +17,20 @@ class Person
     @parent_permission || of_age?
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
