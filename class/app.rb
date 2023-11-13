dd85afc87504ab06df8090d659598102d0ed14c4
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-11-13 16:38:34 +0200

    Make required changes

--- class/app.rb
+++ class/app.rb
@@ -22,9 +22,9 @@ class App
     person_type = gets.chomp.to_s
     case person_type
     when 's'
-      create_student
+      create_student(person_type)
     when 't'
-      create_teacher
+      create_teacher(person_type)
     else
       puts 'Invalid input. Please enter a valid letter.'
       execute(3)
@@ -32,63 +32,96 @@ class App
     puts 'Person created successfully.'
   end
 
-  def self.create_student
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp.to_s
-    print 'Has parent permission ? [Y/N]: '
-    choice = gets.chomp
-    if %w[Y y].include?(choice)
-      Student.new(age, name, parent_permission: true)
-    elsif %w[N n].include?(choice)
-      Student.new(age, name, parent_permission: false)
-    end
+  def self.create_student(person_type)
+    age, name = get_user_input('Student')
+    print 'Has parent permission? (Y/N): '
+    parent_permission = gets.chomp.downcase == 'y'
+    Student.new(age, name: name, parent_permission: parent_permission)
   end
 
-  def self.create_teacher
-    print 'Age: '
-    teacher_age = gets.chomp.to_i
-    print 'Name: '
-    teacher_name = gets.chomp.to_s
+  def self.create_teacher(person_type)
+    age, name = get_user_input('Teacher')
     print 'Specialization: '
-    specialization = gets.chomp.to_s
-    Teacher.new(specialization, teacher_age, name: teacher_name)
+    specialization = gets.chomp
+    Teacher.new(specialization, age, name: name)  
+  end
+
+  def self.get_user_input(person_type)
+    print "Age for #{person_type}: "
+    age = gets.chomp.to_i
+    print "Name for #{person_type}: "
+    name = gets.chomp.to_s
+    [age, name]
   end
 
   def self.create_book
+    title, author = get_book_info
+    new_book = Book.new(title, author)
+    puts 'Book created successfully.'
+    new_book
+  end
+
+  def self.get_book_info
     print 'Title: '
     title = gets.chomp.to_s
     print 'Author: '
     author = gets.chomp.to_s
-    Book.new(title, author)
-    puts 'Book created successfully.'
+    [title, author]
   end
 
   def self.create_rental
-    puts 'Please select a book from the following list by number'
-    Book.all.map.with_index do |book, index|
-      puts "(#{index + 1}) Title: #{book.title}, Author: #{book.author}"
-    end
+    display_books_for_rental
     rental_book_index = gets.chomp.to_i
-    rental_person = Person.all[rental_book_index - 1]
+    rental_person = select_person(rental_book_index)
 
-    print 'Date: '
-    date = gets.chomp
+    date = get_rental_date
     Rental.new(date, rental_person, rental_book_index)
     puts 'Rental created successfully.'
   end
 
-  def self.list_rentals
-    print 'ID of person: '
-    id = gets.chomp.to_i
-    person = Person.all.select { |x| x.id == id } [0]
+  def self.display_books_for_rental
+    puts 'Please select a book from the following list by number:'
+    Book.all.each_with_index do |book, index|
+      puts "(#{index + 1}) Title: #{book.title}, Author: #{book.author}"
+    end
+  end
 
+  def self.select_person(rental_book_index)
+    Person.all[rental_book_index - 1]
+  end
+
+  def self.get_rental_date
+    print 'Date: '
+    gets.chomp
+  end
+
+  def self.list_rentals
+    id = get_person_id
+    display_person_rentals(id)
+  end
+
+  def self.get_person_id
+    print 'ID of person: '
+    gets.chomp.to_i
+  end
+
+  def self.display_person_rentals(id)
+    person = find_person_by_id(id)
     if person
-      puts 'Rentals:'
-      person.rentals.each { |rental| puts "Date: #{rental.date}, Book: #{rental.book.title}" }
+      display_rentals(person)
     else
       puts 'Person not found.'
+    end
+  end
+
+  def self.find_person_by_id(id)
+    Person.all.find { |person| person.id == id }
+  end
+
+  def self.display_rentals(person)
+    puts 'Rentals:'
+    person.rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title}"
     end
   end
 end
