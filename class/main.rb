db3aee7c174e3607739ae0774b8e28e7ba58414f
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-15 22:38:52 +0530

    changes

--- class/main.rb
+++ class/main.rb
@@ -5,11 +5,11 @@ class Main
   def menu
     puts 'Please choose an option by entering a number: '
     load_data
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
+    puts '1 - List books'
+    puts '2 - List people'
+    puts '3 - Create person'
+    puts '4 - Create book'
+    puts '5 - Create rental'
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
     user_choice = gets.chomp.to_i
