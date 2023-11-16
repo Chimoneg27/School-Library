f60cd9ab737d2f39bb105d34e5db741579025b17
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-16 23:11:50 +0530

    Rubocop error fix

--- class/spec/book_spec.rb
+++ class/spec/book_spec.rb
@@ -2,28 +2,28 @@ require 'spec_helper'
 
 describe Book do
   before :each do
-    @book = Book.new "title", "author"
+    @book = Book.new 'title', 'author'
   end
 
-  describe "#new" do
-    it "takes two parameters and returns a Book object" do
+  describe '#new' do
+    it 'takes two parameters and returns a Book object' do
       @book.should be_an_instance_of Book
     end
 
-    it "throws an ArgumentError when given fewer than 2 parameters" do
-        lambda { Book.new "title" }.should raise_exception ArgumentError
+    it 'throws an ArgumentError when given fewer than 2 parameters' do
+      -> { Book.new 'title' }.should raise_exception ArgumentError
     end
   end
 
-  describe "#title" do
-    it "returns the correct title" do
-        @book.title.should eql "title"
+  describe '#title' do
+    it 'returns the correct title' do
+      @book.title.should eql 'title'
     end
   end
 
-  describe "#author" do
-    it "returns the correct author" do
-        @book.author.should eql "author"
+  describe '#author' do
+    it 'returns the correct author' do
+      @book.author.should eql 'author'
     end
   end
 end
