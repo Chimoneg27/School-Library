5be334fbb32fa087ee66db78cc4237e37dca547a
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-17 17:10:10 +0530

    Add tests for fizzbuzz

--- class/spec/solver_spec.rb
+++ class/spec/solver_spec.rb
@@ -12,4 +12,24 @@ describe Solver do
     result = solver.reverse('hello')
     expect(result).to eq('olleh')
   end
+
+  describe '#fizzbuzz' do
+    context 'when applying the FizzBuzz rules' do
+      it 'returns "fizz" for multiples of 3' do
+        expect(subject.fizzbuzz(6)).to eq('fizz')
+      end
+
+      it 'returns "buzz" for multiples of 5' do
+        expect(subject.fizzbuzz(10)).to eq('buzz')
+      end
+
+      it 'returns "fizzbuzz" for multiples of both 3 and 5' do
+        expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
+      end
+
+      it 'returns the number as a string for other cases' do
+        expect(subject.fizzbuzz(7)).to eq('7')
+      end
+    end
+  end
 end
--- class/spec/solver_test.rb
+++ class/spec/solver_test.rb
@@ -2,34 +2,6 @@ require 'rspec'
 require_relative '../src/solver'
 
 RSpec.describe Solver do
-  describe '#factorial' do
-    context 'when calculating the factorial' do
-      it 'returns 1 for 0' do
-        expect(subject.factorial(0)).to eq(1)
-      end
-
-      it 'returns 1 for 1' do
-        expect(subject.factorial(1)).to eq(1)
-      end
-
-      it 'returns the correct factorial for a positive integer' do
-        expect(subject.factorial(5)).to eq(120)
-      end
-
-      it 'raises an exception for a negative integer' do
-        expect { subject.factorial(-1) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
-      end
-    end
-  end
-
-  describe '#reverse' do
-    context 'when reversing a word' do
-      it 'reverses the given word' do
-        expect(subject.reverse('hello')).to eq('olleh')
-      end
-    end
-  end
-
   describe '#fizzbuzz' do
     context 'when applying the FizzBuzz rules' do
       it 'returns "fizz" for multiples of 3' do
