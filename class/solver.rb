c24e3d5529179189ee71fa72409c12735c40173c
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-17 17:13:14 +0530

    Write fizzbuzz code

--- class/solver.rb
+++ class/solver.rb
@@ -6,4 +6,11 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+    num.to_s
+  end
 end
